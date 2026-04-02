76396af2762dd77332be7db08725ee05a8653a2c
Ankit Tiwari <ankittiwari70651@gmail.com>
2026-04-02 22:52:39 +0530

    Stop theme sync ping pong

--- src/app/components/ThemeSynchronizer.tsx
+++ src/app/components/ThemeSynchronizer.tsx
@@ -5,10 +5,9 @@ import { useAppStore } from "../store/useAppStore";
 export function ThemeSynchronizer() {
   const { setTheme, resolvedTheme } = useTheme();
   const storedTheme = useAppStore((state) => state.theme);
-  const setThemePreference = useAppStore((state) => state.setTheme);
 
   useEffect(() => {
-    if (storedTheme) {
+    if (storedTheme && resolvedTheme !== storedTheme) {
       console.info("[Finora theme debug] ThemeSynchronizer apply store theme", {
         storedTheme,
         resolvedTheme,
@@ -16,16 +15,6 @@ export function ThemeSynchronizer() {
       setTheme(storedTheme);
     }
   }, [storedTheme, setTheme]);
-
-  useEffect(() => {
-    if (resolvedTheme && resolvedTheme !== storedTheme) {
-      console.info("[Finora theme debug] ThemeSynchronizer sync resolved theme", {
-        storedTheme,
-        resolvedTheme,
-      });
-      setThemePreference(resolvedTheme as "light" | "dark");
-    }
-  }, [resolvedTheme, storedTheme, setThemePreference]);
 
   return null;
 }
